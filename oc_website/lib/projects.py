49a8524d337281e0949c99ef8a74900944d32730
rr- <rr-@sakuya.pl>
2019-07-01 21:15:33 +0200

    site: refactor project template

--- oc_website/lib/projects.py
+++ oc_website/lib/projects.py
@@ -9,6 +9,7 @@ from oc_website.lib.jinja_env import get_jinja_env
 class Project:
     title: str
     stem: str
+    release_filter: str
     is_finished: bool
 
     @property
@@ -24,10 +25,16 @@ def get_projects() -> T.Iterable[Project]:
         context = template.new_context()
         title = "".join(template.blocks["project_title"](context))
         status = "".join(template.blocks["project_status"](context))
+        release_filter = "".join(
+            template.blocks["project_release_filter"](context)
+        )
 
         if status not in ("finished", "ongoing"):
             raise ValueError(f'Unknown status "{status}" in project "{path}"')
 
         yield Project(
-            title=title, stem=path.stem, is_finished=status == "finished"
+            title=title,
+            stem=path.stem,
+            is_finished=status == "finished",
+            release_filter=release_filter,
         )
