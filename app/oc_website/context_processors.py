0bf3ed80520bf41706cf069e7415494fcd1a2405
Marcin Kurczewski <dash@wind.garden>
2022-12-16 13:53:57 +0100

    update pre-commit

--- app/oc_website/context_processors.py
+++ app/oc_website/context_processors.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 
-def add_settings(request):
+def add_settings(_request):
     return {
         "comments_enabled": settings.COMMENTS_ENABLED,
         "requests_enabled": settings.REQUESTS_ENABLED,
